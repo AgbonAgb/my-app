ee6283f2c82831680fd9d63e3120ea586f8ea097
thaddydore <thaddydore@gmail.com>
2023-09-02 11:15:12 +0100

    w/c: api consumption for create account

--- src/components/signup/form/index.tsx
+++ src/components/signup/form/index.tsx
@@ -2,16 +2,17 @@ import { ChangeEvent, FormEvent, MouseEvent, useState, Fragment } from 'react';
 import styles from './index.module.scss';
 import Input from '../../../custom/input/input';
 import Button from '../../../custom/button/button';
+import { request } from '../../utils/apiCall';
+import { Link } from 'react-router-dom';
 
 interface Person {
 	name: string;
 }
 
 const Form = () => {
-	const [state, setState] = useState('');
-	const [name, setName] = useState('jude');
 	const [error, setError] = useState('');
-
+	const [isLoading, setIsLoading] = useState(false);
+	const [successMessage, setSuccessMessage] = useState('');
 	// const [firstName, setFirsName] = useState('');
 	// const [lastName, setLastName] = useState('');
 	const [PhoneNumber, setPhoneNumber] = useState(''); //Value and action
@@ -28,40 +29,31 @@ const Form = () => {
 		lastName: '',
 	});
 
-	const user = {
-		firstName: person.firstName,
-		lastName: person.lastName,
-		userType: 'MacUser',
-		userName: `${person.firstName}${person.lastName}`,
-		email: EmailAddress,
-		password: Password,
-		phoneNumber:PhoneNumber
-	};
-
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
+		const user = {
+			firstName: person.firstName,
+			lastName: person.lastName,
+			userType: 'Applicant',
+			userName: `${person.firstName}${person.lastName}`,
+			email: EmailAddress,
+			password: Password,
+		};
+
+		setIsLoading(true);
+		setError('');
+		setSuccessMessage('');
+
 		try {
-			//http://myreacttest.mjdtech.ng:1977/api/Authentication/RegisterUser
-			const response = await fetch('https://localhost:5001/api/Authentication/RegisterUser', {
-				method: 'POST',
-				body: JSON.stringify(user),
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
+			(await request.post('/Authentication/RegisterUser', user)) as Person;
 
-			const result = (await response.json()) as Person;
-
-			console.log(result);
+			setSuccessMessage('User has been successfully registered');
 		} catch (error: any) {
 			setError(error?.response?.data?.message ?? error?.mesage);
+		} finally {
+			setIsLoading(false);
 		}
-	};
-
-	const handleButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
-		e.stopPropagation();
-		setName('Mr BAT');
 	};
 
 	//generic
@@ -99,20 +91,12 @@ const Form = () => {
 		const { value } = e.target;
 		setPassword(value);
 	};
+
 	//handleConfirmPassword
 	const handleConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
 		const { value } = e.target;
 		setConfirmPassword(value);
 	};
-
-	console.log({
-		PhoneNumber,
-		EmailAddress,
-		ConfirmPassword,
-		Password,
-		Firstname: person.firstName,
-		LastName: person.lastName,
-	});
 
 	return (
 		<Fragment>
@@ -145,9 +129,14 @@ const Form = () => {
 						value={ConfirmPassword}
 					/>
 				</section>
-				<Button text='SIGN UP' className={styles.btn} onClick={handleButtonClick} />
+				<Button text={isLoading ? 'Sign up...' : 'SIGN UP'} className={styles.btn} disabled={isLoading} />
+				{error && <p className='error'>{error}</p>}
+				{successMessage && <p className='success'>{successMessage}</p>}
 				<p className={styles.para}>
-					Already have an account?<span className={styles.span}>SIGN IN</span>
+					Already have an account?
+					<Link to='/login' className={styles.span}>
+						SIGN IN
+					</Link>
 				</p>
 			</form>
 		</Fragment>
